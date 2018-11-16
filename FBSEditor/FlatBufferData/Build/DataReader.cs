23f8cd0abe131128c2bed861dbe131db77356680
chw <chw-chengwei@foxmail.com>
2018-11-17 07:37:04 +0800

    抽象读取器基类

---
+++ FBSEditor/FlatBufferData/Build/DataReader.cs
@@ -0,0 +1,40 @@
+﻿using FlatBufferData.Model;
+using System;
+
+namespace FlatBufferData.Build
+{
+    public class DataReader
+    {
+        public DataReaderFactory Factory { get; set; }
+
+        public int ErrorLogCount { get; private set; }
+
+        protected void AddError(string path,string text,int line,int column)
+        {
+            ErrorLogCount++;
+
+            if(Factory!=null)
+                Factory.AddError(path, text, line, column);
+        }
+
+        public virtual void Read(Table schema)
+        {
+            throw new Exception("未实现的功能!");
+        }
+
+        public virtual void Read(Table schema, string text)
+        {
+            throw new Exception("未实现的功能!");
+        }
+
+        public virtual void Read(Struct schema)
+        {
+            throw new Exception("未实现的功能!");
+        }
+
+        public virtual void Read(Struct schema, string text)
+        {
+            throw new Exception("未实现的功能!");
+        }
+    }
+}
---
+++ FBSEditor/FlatBufferData/Build/DataReaderFactory.cs
@@ -0,0 +1,79 @@
+﻿using FlatBufferData.Model;
+using FlatBufferData.Model.Attributes;
+
+namespace FlatBufferData.Build
+{
+    public class DataReaderFactory
+    {
+        public delegate void ErrorHandler(string project, string path, string text, int line, int column);
+
+        private string projectName;
+        private ErrorHandler errorHandler;
+        private int errorCount;
+
+        public DataReaderFactory(string projectName, ErrorHandler errorHandler)
+        {
+            this.projectName = projectName;
+            this.errorHandler = errorHandler;
+        }
+
+        public void AddError(string path, string text, int line, int column)
+        {
+            errorHandler?.Invoke(projectName, path, text, line, column);
+        }
+
+
+        public void ReadData(FBSFile file)
+        {
+            if (file.RootTable == null) return;
+
+            var xls = file.RootTable.Attributes.GetAttribte<XLS>();
+            if (xls != null)
+            {
+                var reader = new XLSReader();
+                reader.Factory = this;
+                reader.Read(file.RootTable);
+            }
+        }
+
+        public void ReadData(Table schema)
+        {
+            var reader = GetReader(schema.Attributes);
+            if (reader != null)
+                reader.Read(schema);
+        }
+
+        public void ReadData(Table schema, string text)
+        {
+            var reader = GetReader(schema.Attributes);
+            if (reader != null)
+                reader.Read(schema, text);
+        }
+
+        public void ReadData(Struct schema)
+        {
+            var reader = GetReader(schema.Attributes);
+            if (reader != null)
+                reader.Read(schema);
+        }
+
+        public void ReadData(Struct schema, string text)
+        {
+            var reader = GetReader(schema.Attributes);
+            if (reader != null)
+                reader.Read(schema, text);
+        }
+
+        protected virtual DataReader GetReader(AttributeInfo attributes)
+        {
+            DataReader reader = null;
+            var xls = attributes.GetAttribte<XLS>();
+            if (xls != null)
+            {
+                reader = new XLSReader();
+                reader.Factory = this;
+            }
+            return reader;
+        }
+    }
+}
